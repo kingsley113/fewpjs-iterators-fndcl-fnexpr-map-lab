5fd4b73fe493ccdb323053e9f091a20593bc08a9
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-14 18:42:39 -0700

    commit tree 834c419d820cd3794ed06b1172105c76d3d8b7b6

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
-}
+  const correctedTutorials = tutorials.map((title) => {
+    return title
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(" ");
+  });
+  return correctedTutorials;
+};
